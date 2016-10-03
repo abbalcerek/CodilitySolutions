fcb9a899a4fc8e5eab160c6050b6bdbefbd416e3
adam-pc <abbalcerek@gmail.com>
2016-10-03 23:11:32 +0200

    simple onece implementation

--- src/org/blah/codility/prefixsums/CountDiv.java
+++ src/org/blah/codility/prefixsums/CountDiv.java
@@ -4,4 +4,10 @@ package org.blah.codility.prefixsums;
  * Created by adam on 03.10.16.
  */
 public class CountDiv {
+
+    public int solution(int A, int B, int K) {
+        int before = A % K == 0 ? A / K - 1 : A / K;
+        return B / K - before;
+    }
+
 }
--- src/org/blah/codility/prefixsums/PassingCars.java
+++ src/org/blah/codility/prefixsums/PassingCars.java
@@ -4,4 +4,24 @@ package org.blah.codility.prefixsums;
  * Created by adam on 03.10.16.
  */
 public class PassingCars {
+
+    public int solution(int[] A) {
+
+        int maxValue = 1000000000;
+
+        int zerosSoFar = 0;
+        long result = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            int element = A[i];
+            if (element == 0) {
+                zerosSoFar += 1;
+            } else {
+                result += zerosSoFar;
+            }
+        }
+
+        return maxValue < result ? -1 : (int)result;
+    }
+
 }
